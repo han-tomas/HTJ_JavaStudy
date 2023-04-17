f57fc08a3d7c4a039ba1c1770b77dbeb48d68133
hantaejun <plushan98@naver.com>
2023-04-17 17:31:52 +0900

    2023/04/17(Mon)

---
+++ Chapter5_접근지정어_2/src/com/sist/music/GenieMusic.java
@@ -0,0 +1,7 @@
+package com.sist.music;
+// 데이터형 (기본형과 같다.) 
+public class GenieMusic  {
+	private Music music = new Music();
+
+	
+}
---
+++ Chapter5_접근지정어_2/src/com/sist/music/MelonMusic.java
@@ -0,0 +1,10 @@
+package com.sist.music;
+
+public class MelonMusic {  //public class MelonMusic extends Music  ==> 상속
+
+	private Music music = new Music();
+	private String like;
+	
+	
+	
+}
